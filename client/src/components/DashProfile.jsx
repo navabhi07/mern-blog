8409fae35b19bcf781666b4aac7c1c8f033b1213
kumar Abhinav <krabhinav023@gmail.com>
2024-03-20 11:52:31 +0530

    complete  update user profile page functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -11,18 +11,28 @@ import {
   import { app } from '../firebase';
   import { CircularProgressbar } from 'react-circular-progressbar';
    import 'react-circular-progressbar/dist/styles.css';
+   import {
+    updateStart,
+    updateSuccess,
+    updateFailure,
+    
+  } from '../redux/user/userSlice';
+  import { useDispatch } from 'react-redux';
+  import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-    const {currentUser}=useSelector((state)=>state.user);
+    const {currentUser,error,loading}=useSelector((state)=>state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
-   
-    console.log(imageFileUploadProgress,imageFileUploadError);
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
 
-
-    const filePickerRef = useRef();
+    const [formData,setFormData]=useState({});
+   const filePickerRef = useRef();
+   const dispatch = useDispatch();
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         if (file) {
@@ -82,15 +92,60 @@ uploadTask.on(
     {
        getDownloadURL(uploadTask.snapshot.ref).then((downloadURl)=>{
           setImageFileUrl(downloadURl);
+
+          setFormData({...formData,profilePicture:downloadURl});
+          setImageFileUploading(false);
        });
     }
 );
 };
 
+
+const handleChange=(e)=>{
+  setFormData({...formData,[e.target.id]:e.target.value});
+};
+
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  setUpdateUserError(null);
+  setUpdateUserSuccess(null);
+  if (Object.keys(formData).length === 0) {
+    setUpdateUserError('No changes made');
+    return;
+  }
+  if (imageFileUploading) {
+    setUpdateUserError('Please wait for image to upload');
+    return;
+  }
+  try {
+    dispatch(updateStart());
+    const res = await fetch(`/api/user/update/${currentUser._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      dispatch(updateFailure(data.message));
+      setUpdateUserError(data.message);
+    } else {
+      dispatch(updateSuccess(data));
+      setUpdateUserSuccess("User's profile updated successfully");
+    }
+  } catch (error) {
+    dispatch(updateFailure(error.message));
+    setUpdateUserError(error.message);
+  }
+};
+
+
   return (
     <div className='mx-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl '>Profile</h1>
-        <form  className='flex flex-col gap-4'>
+        <form  onSubmit={handleSubmit} className='flex flex-col gap-4'>
 
            <input 
            type="file" 
@@ -149,18 +204,24 @@ uploadTask.on(
              type='text'
               id='username' 
               placeholder='username' 
-            defaultValue={currentUser.username}/>
+            defaultValue={currentUser.username}
+            onChange={handleChange}
+            />
 
              <TextInput 
              type='email' 
              id='email' 
              placeholder='email' 
-            defaultValue={currentUser.email}/>
+            defaultValue={currentUser.email}
+            onChange={handleChange}
+            />
 
              <TextInput 
              type='password' 
              id='password' 
-             placeholder='password'/>
+             placeholder='password'
+             onChange={handleChange}
+             />
 
              <Button type='submit' gradientDuoTone='purpleToBlue' outline>Update</Button>
         </form>
@@ -174,6 +235,18 @@ uploadTask.on(
                 Sign Out
             </span>
         </div>
+        {updateUserSuccess&&(
+          <Alert color='success' className='mt-5'>
+            {updateUserSuccess}
+          </Alert>
+        )}
+         {updateUserError&&(
+          <Alert color='failure' className='mt-5'>
+            {updateUserError}
+          </Alert>
+        )}
+
+        
     </div>
-  )
+  );
 }
