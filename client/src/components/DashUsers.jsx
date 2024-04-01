187d27c07c63afcfacca56161985df728e71297c
kumar Abhinav <krabhinav023@gmail.com>
2024-04-01 20:07:50 +0530

    add delete functionality to the comment section

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -63,6 +63,7 @@ export default function DashUsers() {
     }
   };
 
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && users.length > 0 ? (
