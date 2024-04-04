e9ef3b4df116bc28f5f01fa8847bafff66b6bfd6
kumar Abhinav <krabhinav023@gmail.com>
2024-04-04 17:27:53 +0530

    no changess dummy

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -10,5 +10,6 @@ router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 
 export default router;
